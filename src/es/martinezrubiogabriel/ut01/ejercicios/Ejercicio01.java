bad5f55dac5314494fdb738439f71d2597329601
GabrielMartinezRubio <64310532+GabrielMartinezRubio@users.noreply.github.com>
2026-09-22 10:30:20 +0200

    Proyectos de Clase, ejercicio 01 del UT01

---
+++ src/es/martinezrubiogabriel/ut01/ejercicios/Ejercicio01.java
@@ -0,0 +1,13 @@
+package es.martinezrubiogabriel.ut01.ejercicios;
+
+public class Ejercicio01 {
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 4;
+		int c = 9;
+		
+		int numeroMayor = (a > b) ? ((a > c) ? a : c) : (b > c) ? b : c;
+		
+		System.out.println("El número más alto en este caso es " +numeroMayor);
+	}
+}
