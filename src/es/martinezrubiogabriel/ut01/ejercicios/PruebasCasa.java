74ce40d3a82ffd82590508d16101d1af037ada0c
GabrielMartinezRubio <64310532+GabrielMartinezRubio@users.noreply.github.com>
2026-09-22 11:34:51 +0200

    Pruebas Casa

---
+++ src/es/martinezrubiogabriel/ut01/ejercicios/PruebasCasa.java
@@ -0,0 +1,14 @@
+package es.martinezrubiogabriel.ut01.ejercicios;
+
+public class PruebasCasa {
+	public static void main(String[] args) {
+		int a = 9;
+		int b = 5;
+		int c = 11;
+		
+		int mayorNumero = a > b ? (a > c ? a : c) : (b > c ? b : c) ;
+		
+		System.out.println(mayorNumero);
+		
+	}
+}
