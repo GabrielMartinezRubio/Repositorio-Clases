92ef15c658e682d451a19b2935fc8309e513e1e8
GabrielMartinezRubio <64310532+GabrielMartinezRubio@users.noreply.github.com>
2026-09-22 13:16:28 +0200

    ejemplo ataque

--- src/es/martinezrubiogabriel/ut01/ejercicios/PruebasCasa.java
+++ src/es/martinezrubiogabriel/ut01/ejercicios/PruebasCasa.java
@@ -5,17 +5,25 @@ public class PruebasCasa {
 		int a = 9;
 		int b = 5;
 		int c = 11;
+		boolean turno = false;
+		int mayorNumero = a > b ? (a > c ? a : c) : (b > c ? b : c);
 		
-		int mayorNumero = a > b ? (a > c ? a : c) : (b > c ? b : c) ;
 		
-		System.out.println(mayorNumero);
+		//System.out.println(mayorNumero);
 		
 		int [] nume = {1,2,3};
+		if (turno == false) {
+			a -=atacar(10);
+			System.out.println(a);			
+		}
 		
-	    for (int numero : nume)
+	    /*for (int numero : nume)
 	    {
-	        System.out.println(numero);
-	    }
+	       System.out.println(numero);
+	    }*/
 		
+	}
+	public static int atacar (int ataque) {
+		return ataque;
 	}
 }
